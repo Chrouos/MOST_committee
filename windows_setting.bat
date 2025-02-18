9a86dd88187fef8aa241ec4a984c67306a6fa413
Chrouos <chrbezz0487@gmail.com>
2025-02-18 17:55:44 +0800

    Update python.exe, windows.bat, and windows_setting.bat

--- windows_setting.bat
+++ windows_setting.bat
@@ -1,7 +1,15 @@
 @echo off
+@chcp 65001 >nul
+REM 啟動虛擬環境
+call .\venv\Scripts\activate
 
-REM Start the python script
-python.exe settingGUI.py .
+REM 檢查虛擬環境 Python 路徑
+echo 正在檢查 Python 執行路徑...
+.\venv\Scripts\python.exe -c "import sys; print(sys.executable)"
 
-REM Pause the script
+REM 執行 settingGUI.py 腳本
+echo 正在執行 settingGUI.py 腳本...
+.\venv\Scripts\python.exe settingGUI.py .
+
+REM 提示執行結束
 pause
