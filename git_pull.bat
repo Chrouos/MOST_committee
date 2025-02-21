dd2e4cc0c31564596df541fe196d7babd1263111
Chrouos <chrbezz0487@gmail.com>
2025-02-21 15:43:36 +0800

    更新git_pull.bat

--- git_pull.bat
+++ git_pull.bat
@@ -3,6 +3,11 @@ cd /d %~dp0
 
 :: 丟棄所有本地更改並強制拉取遠端代碼
 git reset --hard HEAD
+
+:: 更改遠端 URL 為 HTTPS
+git remote set-url origin https://github.com/Chrouos/MOST_committee.git
+
+:: 拉取最新代碼
 git pull
 
 pause
