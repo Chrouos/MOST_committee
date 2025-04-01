58e7450003f74bf36416b3c878e30fad8989b1e9
黃懷萱 <56072039+Chrouos@users.noreply.github.com>
2025-04-01 16:19:58 +0800

    Update git_pull.bat

--- git_pull.bat
+++ git_pull.bat
@@ -4,6 +4,9 @@ cd /d %~dp0
 :: Drop the all changes in the working directory
 git reset --hard HEAD
 
+:: Delete all untrack data and folder
+git clean -fd
+
 :: Change the remote URL to HTTPS
 git remote set-url origin https://github.com/Chrouos/MOST_committee.git
 
